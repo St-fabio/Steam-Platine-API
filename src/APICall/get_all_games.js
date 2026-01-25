23c72acc6168bdf5c3233e91860a0b82f4fef165
Fabio BRAMBILLA <fabio.brambilla@etu.univ-grenoble-alpes.fr>
2026-01-25 12:09:00 +0100

    fixing Steams API functions

--- src/APICall/get_all_games.js
+++ src/APICall/get_all_games.js
@@ -1,9 +1,9 @@
 import 'dotenv/config';
 
-const API_KEY= process.env.API_KEY
+const API_KEY= process.env.STEAM_API_KEY
 
 async function get_all_games(steamid) {
-    const url = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${API_KEY}&steamid=${steamid}&include_appinfo=true`;
+    const url = `https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${API_KEY}&steamid=${steamid}&include_appinfo=true`;
     const response = await fetch(url);
     const data = await response.json();
     return data.response.games;
--- src/APICall/get_player_achievement.js
+++ src/APICall/get_player_achievement.js
@@ -1,9 +1,9 @@
 import 'dotenv/config';
 
-const API_KEY= process.env.API_KEY
+const API_KEY= process.env.STEAM_API_KEY
 
 async function get_player_achievement(appid, steamid) {
-    const url = `http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=${appid}&key=${API_KEY}&steamid=${steamid}`
+    const url = `https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=${appid}&key=${API_KEY}&steamid=${steamid}`
 
     const response = await fetch(url)
 
